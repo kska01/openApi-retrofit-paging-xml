af42d27326a600ba8af5354f61e177317c78788f
kska01 <insideka01@gmail.com>
2024-07-14 00:19:31 +0900

    feat: button에 animailListFragment로 이동하는 클릭 리스너 설정

--- app/src/main/java/com/example/retrofitdrill/FirstScreenFragment.kt
+++ app/src/main/java/com/example/retrofitdrill/FirstScreenFragment.kt
@@ -5,16 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.example.retrofitdrill.databinding.FragmentFirstScreenBinding
 
 
 class FirstScreenFragment : Fragment() {
 
+    private var _binding: FragmentFirstScreenBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first_screen, container, false)
+    ): View {
+        _binding = FragmentFirstScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setClickListener()
+    }
+
+    private fun setClickListener() {
+        binding.btnGoList.setOnClickListener {
+            it.findNavController().navigate(R.id.action_firstScreenFragment_to_animalListFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }

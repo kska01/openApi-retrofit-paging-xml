7f26f7136ffaa853823564b7f20f6672a6be1c06
kska01 <insideka01@gmail.com>
2024-07-19 21:11:56 +0900

    add: ui & network package 추가 및 분리

--- app/src/main/java/com/example/retrofitdrill/ui/AnimalListFragment.kt
+++ app/src/main/java/com/example/retrofitdrill/ui/AnimalListFragment.kt
@@ -1,10 +1,11 @@
-package com.example.retrofitdrill
+package com.example.retrofitdrill.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.retrofitdrill.R
 
 
 class AnimalListFragment : Fragment() {
--- app/src/main/java/com/example/retrofitdrill/ui/FirstScreenFragment.kt
+++ app/src/main/java/com/example/retrofitdrill/ui/FirstScreenFragment.kt
@@ -1,12 +1,12 @@
-package com.example.retrofitdrill
+package com.example.retrofitdrill.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import com.example.retrofitdrill.R
 import com.example.retrofitdrill.databinding.FragmentFirstScreenBinding
 
 

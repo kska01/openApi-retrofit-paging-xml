2c1d6ac2f7e4755cf87b07b2d2b7a219e5c8d891
kska01 <insideka01@gmail.com>
2024-07-26 16:19:03 +0900

    feat: 각 파일에 databinding layout root tag 추가 및 binding class 초기화

--- app/src/main/java/com/example/retrofitdrill/ui/FirstScreenFragment.kt
+++ app/src/main/java/com/example/retrofitdrill/ui/FirstScreenFragment.kt
@@ -1,10 +1,10 @@
 package com.example.retrofitdrill.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.retrofitdrill.R
 import com.example.retrofitdrill.databinding.FragmentFirstScreenBinding

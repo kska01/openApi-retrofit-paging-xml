077674ec0c0f98891b4c9d69a2100d030e22f931
kska01 <insideka01@gmail.com>
2024-07-26 16:16:47 +0900

    feat: recyclerview에 대한 bindingAdapters 추가 및 기능 구현(item layout 제외)

--- app/src/main/java/com/example/retrofitdrill/BindingAdapters.kt
+++ app/src/main/java/com/example/retrofitdrill/BindingAdapters.kt
@@ -1,2 +1,35 @@
 package com.example.retrofitdrill
 
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.retrofitdrill.network.model.Item
+import com.example.retrofitdrill.ui.AnimalListRVAdapter
+
+
+@BindingAdapter("animalList")
+fun setAnimalList(recyclerView: RecyclerView, data: List<Item>?) {
+    val adapter = recyclerView.adapter as? AnimalListRVAdapter
+    adapter?.submitList(data)
+}
+
+
+
+@BindingAdapter("onScrollListener")
+fun setOnScrollListener(recyclerView: RecyclerView, loadMore: () -> Unit) {
+    recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            val layoutManager = recyclerView.layoutManager as GridLayoutManager
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+
+            if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount
+                && firstVisibleItemPosition >= 0
+            ) {
+                loadMore()
+            }
+        }
+    })
+}
--- app/src/main/java/com/example/retrofitdrill/ui/AnimalListFragment.kt
+++ app/src/main/java/com/example/retrofitdrill/ui/AnimalListFragment.kt
@@ -1,15 +1,14 @@
 package com.example.retrofitdrill.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.example.retrofitdrill.R
 import com.example.retrofitdrill.databinding.FragmentAnimalListBinding
-
 
 class AnimalListFragment : Fragment() {
 
@@ -22,43 +21,52 @@ class AnimalListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentAnimalListBinding.inflate(inflater, container, false)
+        _binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_animal_list,
+            container,
+            false
+        )
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        setupRecyclerView()
+
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        setupRecyclerView()
-    }
-
     private fun setupRecyclerView() {
-        val animalListAdapter = AnimalListRVAdapter()
-
-        binding.rvAnimalList.apply {
-            adapter = animalListAdapter
-            addOnScrollListener(object : RecyclerView.OnScrollListener() {
-                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                    super.onScrolled(recyclerView, dx, dy)
-                    val layoutManager = layoutManager as GridLayoutManager
-                    val visibleItemCount = layoutManager.childCount
-                    val totalItemCount = layoutManager.itemCount
-                    val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
-
-                    if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount
-                        && firstVisibleItemPosition >= 0
-                    ) {
-                        viewModel.getAnimalInfo()
-                    }
-                }
-            })
-        }
-
-        viewModel.animalList.observe(viewLifecycleOwner) {
-            val animalList = viewModel.animalList.value
-            animalListAdapter.submitList(animalList)
-        }
+        binding.rvAnimalList.adapter = AnimalListRVAdapter()
     }
+
+//    private fun setupRecyclerView() {
+//        val animalListAdapter = AnimalListRVAdapter()
+//
+//        binding.rvAnimalList.apply {
+//            adapter = animalListAdapter
+//            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+//                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+//                    super.onScrolled(recyclerView, dx, dy)
+//                    val layoutManager = layoutManager as GridLayoutManager
+//                    val visibleItemCount = layoutManager.childCount
+//                    val totalItemCount = layoutManager.itemCount
+//                    val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+//
+//                    if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount
+//                        && firstVisibleItemPosition >= 0
+//                    ) {
+//                        viewModel.getAnimalInfo()
+//                    }
+//                }
+//            })
+//        }
+//
+//        viewModel.animalList.observe(viewLifecycleOwner) {
+//            val animalList = viewModel.animalList.value
+//            animalListAdapter.submitList(animalList)
+//        }
+//    }
 
     override fun onDestroyView() {
         super.onDestroyView()

5e5a54a32cd45a4b2b100c83a2c9e04cee5b1e66
kska01 <insideka01@gmail.com>
2024-08-07 23:51:25 +0900

    refactor: DefaultAppContainer에서 설정한 retrofitService di를 전달받기 위해, NetworkAnimalInfoRepository 클래스의 constructor parameter 수정

--- app/src/main/java/com/example/retrofitdrill/data/AnimalInfoRepository.kt
+++ app/src/main/java/com/example/retrofitdrill/data/AnimalInfoRepository.kt
@@ -1,6 +1,6 @@
 package com.example.retrofitdrill.data
 
-import com.example.retrofitdrill.network.AnimalApi
+import com.example.retrofitdrill.network.AnimalsApiService
 import com.example.retrofitdrill.network.model.AnimalResponse
 import retrofit2.Response
 
@@ -8,8 +8,10 @@ interface AnimalInfoRepository {
     suspend fun getAnimalInfo(page: String): Response<AnimalResponse>
 }
 
-class NetworkAnimalInfoRepository() : AnimalInfoRepository {
+class NetworkAnimalInfoRepository(
+    private val animalsApiService: AnimalsApiService
+) : AnimalInfoRepository {
     override suspend fun getAnimalInfo(page: String): Response<AnimalResponse> {
-        return AnimalApi.retrofitService.getAnimalInfo(pageNo = page)
+        return animalsApiService.getAnimalInfo(pageNo = page)
     }
 }

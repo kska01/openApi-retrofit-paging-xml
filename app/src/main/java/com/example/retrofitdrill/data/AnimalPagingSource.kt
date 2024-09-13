cbdd9e7044565079f636a49156e42e2e2d5a6291
kska01 <insideka01@gmail.com>
2024-09-14 00:57:13 +0900

    feat: PagingSource의 load 함수와 getRefreshKey 함수 구현

--- app/src/main/java/com/example/retrofitdrill/data/AnimalPagingSource.kt
+++ app/src/main/java/com/example/retrofitdrill/data/AnimalPagingSource.kt
@@ -4,16 +4,40 @@ import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.example.retrofitdrill.network.AnimalsApiService
 import com.example.retrofitdrill.network.model.Item
+import retrofit2.HttpException
+import java.io.IOException
 
+private const val STARTING_PAGE_INDEX = 1
 class AnimalPagingSource(
-    private val service: AnimalsApiService,
-    private val query: String
+    private val service: AnimalsApiService
 ) : PagingSource<Int, Item>() {
     override fun getRefreshKey(state: PagingState<Int, Item>): Int? {
-        TODO("Not yet implemented")
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Item> {
-        TODO("Not yet implemented")
+        val position = params.key ?: STARTING_PAGE_INDEX
+
+        return try {
+            val response = service.getAnimalInfo(position.toString())
+            val animals = response.body()?.response?.body?.items?.item
+            val nextKey = if (animals?.isEmpty()!!) {
+                null
+            } else {
+                position + 1
+            }
+            LoadResult.Page(
+                data = animals,
+                prevKey = if (position == STARTING_PAGE_INDEX) null else position - 1,
+                nextKey = nextKey
+            )
+        } catch (exception: IOException) {
+            return LoadResult.Error(exception)
+        } catch (exception: HttpException) {
+            return LoadResult.Error(exception)
+        }
     }
 }
